aebf3abccf4ff356820fa7241123654314a8601b
John Grant <2171719+JohnGrant@users.noreply.github.com>
2019-03-29 01:18:48 -0400

    Rever keys file.

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,23 +1,9 @@
 require('dotenv').config();
-const uriBuilder = require('mongo-uri-builder');
 
-const host = 
-  process.env.MONGO_HOST ||
-  "synapseprepcluster-shard-00-00-xfwt7.azure.mongodb.net:27017,synapseprepcluster-shard-00-01-xfwt7.azure.mongodb.net:27017,synapseprepcluster-shard-00-02-xfwt7.azure.mongodb.net:27017";
-const ssl = process.env.MONGO_SSL || true;
-
-const mongoURI = uriBuilder({
-  username: process.env.MONGO_USERNAME,
-  password: process.env.MONGO_PASS,
-  host,
-  database: process.env.MONGO_DATABASE,
-  options: {
-    ssl,
-    retryWrites: true
-  }
-});
-
+console.log(process.env.MONGO_PASS);
 module.exports = {
-  mongoURI,
+  mongoURI: `mongodb://${process.env.MONGO_USERNAME}:${
+    process.env.MONGO_PASS
+  }@synapseprepcluster-shard-00-00-xfwt7.azure.mongodb.net:27017,synapseprepcluster-shard-00-01-xfwt7.azure.mongodb.net:27017,synapseprepcluster-shard-00-02-xfwt7.azure.mongodb.net:27017/test?ssl=true&replicaSet=Synapseprepcluster-shard-0&authSource=admin&retryWrites=true`,
   secretOrKey: `${process.env.SECRET_OR_KEY}`
-}
+};
