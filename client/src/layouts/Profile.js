adbe23c981fe5d2fe24c78b14e3d011a2b5158c0
Paul Barber <studentsforchange@live.com>
2019-07-17 22:44:10 -0500

    update book.pdf + fix some response emails

--- client/src/layouts/Profile.js
+++ client/src/layouts/Profile.js
@@ -1,119 +1,115 @@
 // Node modules
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import { withStyles } from "@material-ui/core/styles";
-import classNames from "classnames";
-import ReactFilestack from "filestack-react";
-import { connect } from "react-redux";
-import axios from "axios";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import classNames from 'classnames';
+import ReactFilestack from 'filestack-react';
+import { connect } from 'react-redux';
+import axios from 'axios';
 
 // Redux actions
-import { GET_ERRORS } from "../actions/types";
-import { getCurrentProfile } from "../actions/profileActions";
-import {
-  clearErrors,
-  deleteAccount,
-  updateAccount
-} from "../actions/authActions";
+import { GET_ERRORS } from '../actions/types';
+import { getCurrentProfile } from '../actions/profileActions';
+import { clearErrors, deleteAccount, updateAccount } from '../actions/authActions';
 
 // Local components
-import ButtonCustom from "../assets/jss/components/ButtonCustom";
-import CardCustom from "../assets/jss/components/CardCustom";
+import ButtonCustom from '../assets/jss/components/ButtonCustom';
+import CardCustom from '../assets/jss/components/CardCustom';
 
 // Material UI components
-import Grid from "@material-ui/core/Grid";
-import IconButton from "@material-ui/core/IconButton";
-import InputAdornment from "@material-ui/core/InputAdornment";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import TextField from "@material-ui/core/TextField";
-import Typography from "@material-ui/core/Typography";
-import Visibility from "@material-ui/icons/Visibility";
-import VisibilityOff from "@material-ui/icons/VisibilityOff";
-import Button from "@material-ui/core/Button";
-import Divider from "@material-ui/core/Divider";
+import Grid from '@material-ui/core/Grid';
+import IconButton from '@material-ui/core/IconButton';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
 
 // Local assets
 
 //  Style overrides
 const styles = theme => ({
   progress: {
-    color: "grey"
+    color: 'grey'
   },
   textField: {},
   label: {
-    color: "grey"
+    color: 'grey'
   },
   textFieldRoot: {
-    color: "grey",
-    maxWidth: "300px"
+    color: 'grey',
+    maxWidth: '300px'
   },
   submit: {},
   saveChangesButton: {
-    marginTop: "24px"
+    marginTop: '24px'
   },
   progressContainer: {
-    height: "-webkit-fill-available"
+    height: '-webkit-fill-available'
   },
   topTextFeild: {
-    marginBottom: "15px"
+    marginBottom: '15px'
   },
   billingContainer: {
-    marginTop: "50px"
+    marginTop: '50px'
   },
   profileTextBottom: {
-    marginBottom: "50px"
+    marginBottom: '50px'
   },
   profileCardContainer: {
-    maxWidth: "450px",
-    minWidth: "0px",
-    [theme.breakpoints.up("sm")]: {
-      minWidth: "380px"
+    maxWidth: '450px',
+    minWidth: '0px',
+    [theme.breakpoints.up('sm')]: {
+      minWidth: '380px'
     }
   },
   changePassCardContainer: {
-    maxWidth: "450px",
-    minWidth: "0px",
-    [theme.breakpoints.up("sm")]: {
-      minWidth: "380px"
+    maxWidth: '450px',
+    minWidth: '0px',
+    [theme.breakpoints.up('sm')]: {
+      minWidth: '380px'
     }
   },
   avatar: {
-    borderRadius: "200px",
-    width: "100px",
-    height: "100px"
+    borderRadius: '200px',
+    width: '100px',
+    height: '100px'
   },
   profileName: {
-    backgroundImage: "linear-gradient(224deg, #209cff 0%, #68e0cf 100%)",
-    color: "transparent",
-    "-webkit-background-clip": "text",
-    backgroundClip: "text"
+    backgroundImage: 'linear-gradient(224deg, #209cff 0%, #68e0cf 100%)',
+    color: 'transparent',
+    '-webkit-background-clip': 'text',
+    backgroundClip: 'text'
   },
   welcomeContainer: {
-    marginLeft: "20px"
+    marginLeft: '20px'
   },
   accountInfoContainer: {
-    marginTop: "20px"
+    marginTop: '20px'
   },
   deleteButton: {
-    fontSize: "0.8rem",
-    marginTop: "40px",
-    color: "red",
-    opacity: "0.5"
+    fontSize: '0.8rem',
+    marginTop: '40px',
+    color: 'red',
+    opacity: '0.5'
   },
   changeAvatarButton: {
-    fontSize: "0.6rem",
-    [theme.breakpoints.up("sm")]: {
-      fontSize: "0.8rem"
+    fontSize: '0.6rem',
+    [theme.breakpoints.up('sm')]: {
+      fontSize: '0.8rem'
     }
   }
 });
 
 class Profile extends Component {
   state = {
-    password: "",
+    password: '',
     id: this.props.auth.user.id,
     name: this.props.auth.user.name,
-    password2: "",
+    password2: '',
     errors: {},
     avatar: this.props.auth.user.avatar,
     email: this.props.auth.user.email,
@@ -140,6 +136,14 @@ class Profile extends Component {
     this.setState({
       avatar: newAvatar
     });
+    const updatedUser = {
+      id: this.state.id,
+      name: this.state.name,
+      email: this.state.email,
+      avatar: newAvatar
+    };
+    this.props.updateAccount(updatedUser);
+    console.log('updatedUser', updatedUser);
   };
 
   handleUpdateAccount = () => {
@@ -150,11 +154,11 @@ class Profile extends Component {
       avatar: this.state.avatar
     };
     this.props.updateAccount(updatedUser);
-    console.log("updatedUser", updatedUser);
+    console.log('updatedUser', updatedUser);
   };
 
   onError = error => {
-    console.error("error", error);
+    console.error('error', error);
   };
 
   handleChange = prop => event => {
@@ -172,7 +176,7 @@ class Profile extends Component {
       password2: this.state.password2
     };
     axios
-      .post("/api/users/changepass", passwordData)
+      .post('/api/users/changepass', passwordData)
       .then(() => {
         this.setState({
           updated: true
@@ -192,12 +196,7 @@ class Profile extends Component {
 
     if (profile === null || loading) {
       profileContent = (
-        <Grid
-          container
-          justify="center"
-          alignItems="center"
-          className={classes.progressContainer}
-        >
+        <Grid container justify="center" alignItems="center" className={classes.progressContainer}>
           <CircularProgress className={classes.progress} />
         </Grid>
       );
@@ -210,13 +209,7 @@ class Profile extends Component {
         profileContent = (
           <React.Fragment>
             <Grid container spacing={24} justify="space-around">
-              <Grid
-                className={classes.changePassCardContainer}
-                item
-                xs={12}
-                sm={6}
-                md={5}
-              >
+              <Grid className={classes.changePassCardContainer} item xs={12} sm={6} md={5}>
                 <CardCustom padding title="Change Password" borderBottom>
                   <Grid container justify="center">
                     <form onSubmit={this.handleSubmit} className={classes.form}>
@@ -226,11 +219,11 @@ class Profile extends Component {
                           id="outlined-adornment-password"
                           className={classes.topTextFeild}
                           variant="outlined"
-                          type={this.state.showPassword ? "text" : "password"}
+                          type={this.state.showPassword ? 'text' : 'password'}
                           label="New Password"
                           helperText={errors.password}
                           value={this.state.password}
-                          onChange={this.handleChange("password")}
+                          onChange={this.handleChange('password')}
                           InputProps={{
                             endAdornment: (
                               <InputAdornment position="end">
@@ -238,11 +231,7 @@ class Profile extends Component {
                                   aria-label="Toggle password visibility"
                                   onClick={this.handleClickShowPassword}
                                 >
-                                  {this.state.showPassword ? (
-                                    <VisibilityOff />
-                                  ) : (
-                                    <Visibility />
-                                  )}
+                                  {this.state.showPassword ? <VisibilityOff /> : <Visibility />}
                                 </IconButton>
                               </InputAdornment>
                             )
@@ -252,41 +241,26 @@ class Profile extends Component {
                           error={Boolean(errors.password2)}
                           id="outlined-adornment-password2"
                           classes={{ root: classes.root }}
-                          className={classNames(
-                            classes.margin,
-                            classes.textField
-                          )}
+                          className={classNames(classes.margin, classes.textField)}
                           variant="outlined"
-                          type={this.state.showPassword ? "text" : "password"}
+                          type={this.state.showPassword ? 'text' : 'password'}
                           label="Confirm New Password"
                           value={this.state.password2}
                           helperText={errors.password2}
-                          onChange={this.handleChange("password2")}
+                          onChange={this.handleChange('password2')}
                         />
                       </Grid>
-                      <Grid
-                        container
-                        justify="center"
-                        className={classes.saveChangesButton}
-                      >
+                      <Grid container justify="center" className={classes.saveChangesButton}>
                         {!updated && (
                           <Grid item>
-                            <ButtonCustom
-                              type="submit"
-                              fullWidth
-                              className={classes.submit}
-                            >
+                            <ButtonCustom type="submit" fullWidth className={classes.submit}>
                               Save Changes
                             </ButtonCustom>
                           </Grid>
                         )}
                       </Grid>
                       {updated && (
-                        <Typography
-                          variant="body1"
-                          align="center"
-                          color="textPrimary"
-                        >
+                        <Typography variant="body1" align="center" color="textPrimary">
                           Your password has been changed.
                         </Typography>
                       )}
@@ -295,29 +269,19 @@ class Profile extends Component {
                 </CardCustom>
               </Grid>
 
-              <Grid
-                item
-                xs={12}
-                sm={6}
-                md={5}
-                className={classes.profileCardContainer}
-              >
+              <Grid item xs={12} sm={6} md={5} className={classes.profileCardContainer}>
                 <CardCustom padding title="Profile" borderBottom>
                   <Grid container alignItems="center">
                     <Grid item>
                       <Grid container alignItems="center" direction="column">
-                        <img
-                          src={avatar}
-                          alt="User avatar"
-                          className={classes.avatar}
-                        />
+                        <img src={avatar} alt="User avatar" className={classes.avatar} />
                         <Button className={classes.changeAvatarButton}>
                           <ReactFilestack
-                            apikey={"AovPr36fbS4W9jPnBUTosz"}
+                            apikey={'AovPr36fbS4W9jPnBUTosz'}
                             componentDisplayMode={{
-                              type: "link",
-                              customText: "Change Avatar",
-                              customClass: "some-custom-class"
+                              type: 'link',
+                              customText: 'Change Avatar',
+                              customClass: 'some-custom-class'
                             }}
                             actionOptions={{ transformations: { force: true } }}
                             buttonText="Upload Photo"
@@ -345,23 +309,19 @@ class Profile extends Component {
                   </Grid>
 
                   <Divider />
-                  <Grid
-                    container
-                    justify="center"
-                    className={classes.accountInfoContainer}
-                  >
+                  <Grid container justify="center" className={classes.accountInfoContainer}>
                     <TextField
                       error={Boolean(errors.email)}
                       id="outlined-email"
                       classes={{ root: classes.textFieldRoot }}
                       className={classes.topTextFeild}
                       variant="outlined"
-                      type={"email"}
+                      type={'email'}
                       label="Email"
                       value={email}
                       helperText={errors.email}
                       fullWidth={true}
-                      onChange={this.handleChange("email")}
+                      onChange={this.handleChange('email')}
                     />
                     <TextField
                       error={Boolean(errors.name)}
@@ -373,7 +333,7 @@ class Profile extends Component {
                       value={name}
                       helperText={errors.name}
                       fullWidth={true}
-                      onChange={this.handleChange("name")}
+                      onChange={this.handleChange('name')}
                     />
                     <ButtonCustom
                       className={classes.saveChangesButton}
@@ -383,10 +343,7 @@ class Profile extends Component {
                     </ButtonCustom>
                   </Grid>
                   <Grid container justify="flex-end">
-                    <Button
-                      onClick={this.onDeleteClick}
-                      className={classes.deleteButton}
-                    >
+                    <Button onClick={this.onDeleteClick} className={classes.deleteButton}>
                       Delete My Account
                     </Button>
                   </Grid>
@@ -395,12 +352,7 @@ class Profile extends Component {
             </Grid>
 
             <Grid item xs={12} sm={10} className={classes.billingContainer}>
-              <CardCustom
-                padding
-                title="Billing"
-                borderBottom
-                className={classes.billing}
-              >
+              <CardCustom padding title="Billing" borderBottom className={classes.billing}>
                 <Typography component="h1" variant="body1">
                   Subscription Status: Free
                 </Typography>
