be114f55e64952e94c16e5e3dcf785e6dac4a15e
John Grant <2171719+JohnGrant@users.noreply.github.com>
2019-07-24 02:35:10 -0400

    Add and pass Send email test.

--- server/lamdas/__tests__/emailResetToken.test.js
+++ server/lamdas/__tests__/emailResetToken.test.js
@@ -2,18 +2,26 @@ import request from 'supertest';
 import mongoose from 'mongoose';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import crypto from 'crypto';
+import nodemailer from 'nodemailer';
+import mockedEnv from 'mocked-env';
+import Email from 'email-templates';
+import path from 'path';
 import app from '../emailResetToken';
 import validateResetInput from '../../validation/reset';
 import User from '../../models/User';
 
 jest.mock('../../validation/reset');
+jest.mock('nodemailer', () => ({ createTransport: jest.fn() }));
+jest.mock('email-templates');
+
 const mockToken = '2e54dd4ee3784c21d05d2f570f9e1b948407d4a4';
 const mockDateNow = 1563769101244;
 const expires = new Date(mockDateNow).getTime() + 3600000;
+const emailPassword = 'ASDFASDF';
 
 describe('Reset email token', () => {
   let mongoServer;
-  let env;
+  let restore;
   const email = 'asdf@asdf.com';
 
   const validateResetFalse = () => ({
@@ -29,24 +37,31 @@ describe('Reset email token', () => {
     // Arrange
     mongoServer = new MongoMemoryServer();
     const mongoUri = await mongoServer.getConnectionString();
-    env = { ...env };
-    process.env.MONGODB_URI = mongoUri;
     const opts = { useNewUrlParser: true };
     await mongoose.connect(mongoUri, opts, (err) => {
       if (err) throw new Error(`connection failed: ${err}`);
     });
     await User.deleteMany({});
     await User.create({ email });
+
+    restore = mockedEnv({
+      EMAIL_PASSWORD: emailPassword,
+      MONGODB_URI: mongoUri,
+    });
   });
 
   beforeEach(() => {
-    validateResetInput.mockClear();
+    nodemailer.createTransport.mockClear();
+    validateResetInput.mockReset();
+    Email.mockClear();
   });
 
+  afterEach(() => {});
+
   afterAll(async () => {
-    process.env = env;
     mongoose.disconnect();
     await mongoServer.stop();
+    restore();
   });
 
   it('should return 400 if validation fails.', async () => {
@@ -96,11 +111,58 @@ describe('Reset email token', () => {
     expect(user.resetPasswordToken).toEqual(mockToken);
     expect(user.resetPasswordExpires.getTime()).toEqual(expires);
 
-    randomBytes.mockClear();
-    now.mockClear();
+    randomBytes.mockRestore();
+    now.mockRestore();
   });
 
-  xit('should send email to user', () => {
-    throw new Error('not implemented');
+  it('should send email to user', async () => {
+    // Arrange
+    //
+    validateResetInput.mockImplementation(validateResetTrue);
+
+    const randomBytes = jest.spyOn(crypto, 'randomBytes');
+    randomBytes.mockReturnValueOnce(mockToken);
+
+    const now = jest.spyOn(Date, 'now');
+    now.mockReturnValue(mockDateNow);
+
+    const join = jest.spyOn(path, 'join');
+    join.mockReturnValueOnce('/tests/');
+
+    await request(app)
+      .post('/')
+      .send({ email });
+
+    expect(nodemailer.createTransport.mock.calls[0][0]).toEqual({
+      host: 'smtp.gmail.com',
+      port: 587,
+      secure: false,
+      requireTLS: true,
+      auth: {
+        user: 'synapseprep@gmail.com',
+        pass: emailPassword,
+      },
+    });
+
+    const emailOptions = {
+      template: '/tests/',
+      message: {
+        to: email,
+      },
+      locals: {
+        title: 'Password Reset Request',
+        message:
+          'We just got a request to reset your password.\n\n'
+          + 'Please click the button below, or paste this url into your browser to complete the process:\n\n'
+          + `https://app.synapseprep.net/resetpassword/${mockToken}`,
+        buttonText: 'Reset Password',
+        buttonLink: `https://app.synapseprep.net/resetpassword/${mockToken}`,
+        subject: 'Synapse Prep Password Reset Request',
+      },
+    };
+    expect(Email.mock.instances[0].send.mock.calls[0][0]).toEqual(emailOptions);
+
+    randomBytes.mockRestore();
+    join.mockRestore();
   });
 });
--- server/lamdas/emailResetToken.js
+++ server/lamdas/emailResetToken.js
@@ -55,65 +55,47 @@ async function emailResetToken(req, res) {
     console.error(err);
   }
 
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    auth: {
+      user: 'synapseprep@gmail.com',
+      pass: `${process.env.EMAIL_PASSWORD}`,
+    },
+  });
+
+  const emailObj = new Email({
+    transport: transporter,
+    message: {
+      from: 'support@synapseprep.net',
+    },
+  });
+
+  try {
+    await emailObj.send({
+      template: path.join(__dirname, '..', 'emails'),
+      message: {
+        to: `${user.email}`,
+      },
+      locals: {
+        title: 'Password Reset Request',
+        message:
+          'We just got a request to reset your password.\n\n'
+          + 'Please click the button below, or paste this url into your browser to complete the process:\n\n'
+          + `https://app.synapseprep.net/resetpassword/${token}`,
+        buttonText: 'Reset Password',
+        buttonLink: `https://app.synapseprep.net/resetpassword/${token}`,
+        subject: 'Synapse Prep Password Reset Request',
+      },
+    });
+  } catch (err) {
+    console.error(err);
+  }
+
   res.status(200).json({ emailSent: true });
   res.send();
-
-  // Promise.resolve({})
-  //   .then((user) => {
-  //     // Check for user
-
-  //     const token = crypto.randomBytes(20).toString('hex');
-  //     user.resetPasswordToken = token;
-  //     user.resetPasswordExpires = Date.now() + 3600000;
-  //     user.save((err) => {
-  //       if (err) {
-  //         console.error(err);
-  //       }
-  //     });
-
-  //     const transporter = nodemailer.createTransport({
-  //       host: 'smtp.gmail.com',
-  //       port: 587,
-  //       secure: false,
-  //       requireTLS: true,
-  //       auth: {
-  //         user: 'synapseprep@gmail.com',
-  //         pass: `${process.env.EMAIL_PASSWORD}`,
-  //       },
-  //     });
-
-  //     const email = new Email({
-  //       transport: transporter,
-  //       message: {
-  //         from: 'support@synapseprep.net',
-  //       },
-  //       // uncomment below to send emails in development/test env:
-  //       send: true,
-  //     });
-
-  //     email
-  //       .send({
-  //         template: path.join(__dirname, '..', 'emails'),
-  //         message: {
-  //           to: `${user.email}`,
-  //         },
-  //         locals: {
-  //           title: 'Password Reset Request',
-  //           message:
-  //             'We just got a request to reset your password.\n\n'
-  //             + 'Please click the button below, or paste this url into your browser to complete the process:\n\n'
-  //             + `https://app.synapseprep.net/resetpassword/${token}`,
-  //           buttonText: 'Reset Password',
-  //           buttonLink: `https://app.synapseprep.net/resetpassword/${token}`,
-  //           subject: 'Synapse Prep Password Reset Request',
-  //         },
-  //       })
-  //       .then(() => {
-  //         res.status(200).json({ emailSent: true });
-  //       })
-  //       .catch(err => console.log(err));
-  //   })
-  //   .catch(err => console.log(err));
 }
 
 // @route Get api/users/emailRecoveryToken
